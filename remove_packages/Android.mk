e8d11fa277051b2a52fd4b88a3695301e7c9bf80
Ij Israfil <israfilkhan494@gmail.com>
2023-12-31 18:38:48 +0600

    RMX1901: Re-arrange remove package for better view
    + remove more gapps
    Signed-off-by: ij-israfil <israfilkhan494gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,35 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Drive Maps Photos MeetPrebuilt PrebuiltGmail GoogleCamera AmbientStreaming FM2 Papers AndroidAutoStubPrebuilt Showcase
+LOCAL_OVERRIDES_PACKAGES := \
+		RecorderPrebuilt \
+		Photos \
+		Drive \
+		Maps \
+		YouTube \
+		MeetPrebuilt \
+		PrebuiltGmail \
+		SoundAmplifierPrebuilt \
+		GoogleCamera \
+		AdaptiveVPNPrebuilt \
+		CarrierLocation \
+		CarrierMetrics \
+		DevicePolicyPrebuilt \
+		DiagnosticsToolPrebuilt \
+		SafetyHubPrebuilt \
+		ScribePrebuilt \
+		Showcase \
+		Tycho \
+		Papers \
+		Videos \
+		PixelLiveWallpaperPrebuilt \
+		talkback \
+		Updates \
+		FM2 \
+		NfcNci \
+		ViMusic \
+		AndroidAutoStubPrebuilt \
+		Papers
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
