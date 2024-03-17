30f3e0c3b6c8740840f3c88e3388a8f575995269
Shravankumar <shravankumarsayz@gmail.com>
2024-03-17 10:28:25 +0530

    RMX1901: Blaze

--- blaze_RMX1901.mk
+++ blaze_RMX1901.mk
@@ -13,8 +13,9 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 WITH_GAPPS := true
 TARGET_DISABLE_EPPE := true
+BLAZE_MAINTAINER := SHRAVAN
 
-$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -24,7 +25,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := genesis_RMX1901
+PRODUCT_NAME := blaze_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_SYSTEM_NAME := RMX1901
