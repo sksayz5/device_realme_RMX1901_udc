636a49244764eb72804c17f76be05e21c5e637e2
Shravan Sayz <shravankumarsayz@gmail.com>
2024-04-15 03:49:42 +0000

    RMX1901: Bringup ProjectBlaze

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/genesis_RMX1901.mk
+    $(LOCAL_DIR)/blaze_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    genesis_RMX1901-userdebug \
-    genesis_RMX1901-eng
+    blaze_RMX1901-userdebug \
+    blaze_RMX1901-eng
