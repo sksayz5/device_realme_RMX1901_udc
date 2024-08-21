2891db7baefa81877ba830802da7ba97b19f5a7e
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2024-08-21 11:39:16 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/aospa_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1901-userdebug \
-    lineage_RMX1901-eng
+    aospa_RMX1901-userdebug \
+    aospa_RMX1901-eng
